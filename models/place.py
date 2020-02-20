f5897474e9f01a8f06a62ba790b61579dcf81bcd
FaroukAjimi <benajimi@gmail.com>
2020-02-20 21:33:47 +0000

    lll

--- models/place.py
+++ models/place.py
@@ -6,7 +6,7 @@ class Place(BaseModel):
     city_id = ""
     user_id = ""
     name = ""
-    descritption = ""
+    description = ""
     number_romms = 0
     number_bathrooms = 0
     max_guest = 0
