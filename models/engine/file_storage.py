49337a11a01273146ef484e4d5e25bfbf09b6a81
amineneifer <amineneifer2000@gmail.com>
2020-02-23 18:59:19 +0000

    changed value to v, because it was a wrong name

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,11 +37,9 @@ class FileStorage:
 
     def reload(self):
         """reload function"""
-        try:
-            with open(self.__file_path, 'r') as file:
-                data = json.load(file)
-                for k, v in data.items():
-                    obj = eval(k.split('.')[0])(**value)
-                    self.new(obj)
-        except:
-            pass
+        with open(self.__file_path, 'r') as file:
+            data = json.load(file)
+            print("type :",type(data))
+            for k, v in data.items():
+                obj = eval(k.split('.')[0])(**v)
+                self.new(obj)
